b351e97672fe149daad67c25b5ff0af2680084d3
OwenChambers <owen.chambers2001@gmail.com>
2023-11-15 21:18:03 -0500

    Created controllers and routes and updates express

--- config/config.js
+++ config/config.js
@@ -1,6 +1,7 @@
 const config = {
      env: process.env.NODE_ENV || 'development', 
      port: process.env.PORT || 3000,
+     jwtSecret: process.env.JWT_SECRET || "secret", 
      mongoUri: process.env.MONGODB_URI || "mongodb+srv://meet36043:C4M9nsOEO1vPJ8Ye@projectcomp229.tkgoffw.mongodb.net/"||
     process.env.MONGO_HOST ||
      'mongodb://' + (process.env.IP || 'localhost') + ':' + 
--- config/express.js
+++ config/express.js
@@ -1,3 +1,9 @@
 import express from 'express'
 const app = express();
+app.get('/', (req, res) => {
+    res.status(200).send(Template()) 
+    })
+    //...
+    app.use(express.json());
+    app.use(express.urlencoded({ extended: true }));
 export default app;
