698f8730a1fb9d4313ba99839b1bc1b40cb080ee
APMx350 <alepintado350@gmail.com>
2023-11-16 23:10:26 -0500

    localhost Message Fix

--- server.js
+++ server.js
@@ -17,11 +17,16 @@ app.use(cors());
 
 app.use(bodyParser.json());
 
+
 mongoose.connect(config.mongoUri)
 .then(() => console.log('Connected to MongoDB'))
 .catch(err => console.error('Error to connect to MongoDB', err));
 
 app.use('/api/users', userRoutes);
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to User application." });
+  });
 
 app.listen(config.port, () => {
   console.log(`Server running at port ${config.port}`);
