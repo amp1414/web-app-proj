54a77d76e0414891ef39fd82f1d6983b75d2e8d2
APMx350 <alepintado350@gmail.com>
2023-11-27 22:30:09 -0500

    A
    AAA

--- server.js
+++ server.js
@@ -7,7 +7,11 @@ import cors from 'cors';
 import config from './config/config.js';
 import userRoutes from './server/routes/user.routes.js'; 
 import dotenv from 'dotenv';
-
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 dotenv.config();
 
 const app = express();
@@ -17,16 +21,18 @@ app.use(cors());
 
 app.use(bodyParser.json());
 
-
 mongoose.connect(config.mongoUri)
 .then(() => console.log('Connected to MongoDB'))
 .catch(err => console.error('Error to connect to MongoDB', err));
-
+// Set the views directory and use the ejs engine
+app.set('views', path.join(__dirname, 'server', 'views'));
+app.set('view engine', 'ejs');
 app.use('/api/users', userRoutes);
 
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to User application." });
-  });
+  res.render('index'); // Render the home page
+});
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(config.port, () => {
   console.log(`Server running at port ${config.port}`);
